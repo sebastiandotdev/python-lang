9648a16008f57c3dd311bcc9ec640dcbd0430f44
Sebastian009w <ia3266505@gmail.com>
2023-02-12 18:43:41 -0500

    repasando una list

--- src/11-introduccion-list.py
+++ src/11-introduccion-list.py
@@ -12,6 +12,13 @@ my_list = [20 , 28, 30, 19, 10 , 17]
 print(my_list)
 print(len(my_list))
 
-my_other_list = [18 , 1.69, 'sebastian', 'castro']
+my_other_list = [18 , 1.69, 'sebastian', 'castro', ['list', 'other']]
 print(my_other_list)
 print(len(my_other_list))
+
+print(my_other_list[-1])
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[2])
+print(my_other_list[3])
+print(my_other_list[-1][0])
