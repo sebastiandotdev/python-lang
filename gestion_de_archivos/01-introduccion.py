274366205901b69bba083d9d76441a27a4f981a1
Sebastian009w <ia3266595@gmail.com>
2023-04-27 15:13:11 -0500

    ctime

--- gestion_de_archivos/01-introduccion.py
+++ gestion_de_archivos/01-introduccion.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from time import ctime
 
 archivo = Path('gestion_de_archivos/archivo.txt')
 
 # archivo.exists()
 # archivo.rename()
 # archivo.unlink()
-archivo.stat()
+# print(archivo.stat())
+print("acceso", ctime(archivo.stat().st_atime))
