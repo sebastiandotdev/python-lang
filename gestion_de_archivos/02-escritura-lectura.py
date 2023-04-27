4092bc5f99b1a39703744e7ccad56c6a695aa25c
Sebastian009w <ia3266595@gmail.com>
2023-04-27 15:19:44 -0500

    writer

---
+++ gestion_de_archivos/02-escritura-lectura.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+archivo = Path('gestion_de_archivos/archivo.txt')
+
+texto = archivo.read_text("utf-8").split("\n")
+
+texto.insert(0, 'sebastian')
+
+archivo.write_text("\n".join(texto), 'urf-8')
