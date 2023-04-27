e07e6cf1fa3653ce04d349f6c7341712b64f6f00
Sebastian009w <ia3266595@gmail.com>
2023-04-27 15:51:14 -0500

    leyendo un archivo json

--- gestion_de_archivos/05-json.py
+++ gestion_de_archivos/05-json.py
@@ -2,13 +2,17 @@ import json
 from pathlib import Path
 
 
-products = [
-    {'id': 1, 'name': 'mouse'},
-    {'id': 2, 'name': 'wifi'},
-    {'id': 3, 'name': 'keyboard'},
-]
+# products = [
+#     {'id': 1, 'name': 'mouse'},
+#     {'id': 2, 'name': 'wifi'},
+#     {'id': 3, 'name': 'keyboard'},
+# ]
 
-data = json.dumps(products)
-print(data)
+# data = json.dumps(products)
+# print(data)
 
-Path('gestion_de_archivos/products.json').write_text(data)
+# Path('gestion_de_archivos/products.json').write_text(data)
+
+data = Path('gestion_de_archivos/products.json').read_text(encoding="utf-8")
+products = json.loads(data)
+print(products)
