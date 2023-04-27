bf6441cea0f2f7d4e47f60dfe478a1f0d3c380a9
Sebastian009w <ia3266595@gmail.com>
2023-04-27 15:27:16 -0500

    read

--- gestion_de_archivos/03-open.py
+++ gestion_de_archivos/03-open.py
@@ -2,7 +2,13 @@ from io import open
 
 texto = 'holas'
 
-archivo = open('gestion_de_archivos/hola-archivo.txt', 'w')
+# archivo = open('gestion_de_archivos/hola-archivo.txt', 'w')
 
-archivo.write(texto)
+# archivo.write(texto)
+# archivo.close()
+
+archivo = open('gestion_de_archivos/hola-archivo.txt', 'r')
+
+texto = archivo.read()
 archivo.close()
+print(texto)
