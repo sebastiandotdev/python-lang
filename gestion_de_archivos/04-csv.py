3d46f1577d0e2f2b6312242caa748a511770f3c4
Sebastian009w <ia3266595@gmail.com>
2023-04-27 15:40:16 -0500

    leyendo un archivo csv

--- gestion_de_archivos/04-csv.py
+++ gestion_de_archivos/04-csv.py
@@ -1,7 +1,14 @@
 import csv
 
 
-with open('gestion_de_archivos/archiv.csv', 'w') as archivo:
-    writer = csv.writer(archivo)
-    writer.writerow(['id', 'username', 'lastname'])
-    writer.writerow([1, 'sebastian', 'castro'])
+# with open('gestion_de_archivos/archiv.csv', 'w') as archivo:
+#     writer = csv.writer(archivo)
+#     writer.writerow(['id', 'username', 'lastname'])
+#     writer.writerow([1, 'sebastian', 'castro'])
+
+
+with open('gestion_de_archivos/archiv.csv', 'r') as archivo:
+    reader = csv.reader(archivo)
+
+    for line in reader:
+        print(line)
