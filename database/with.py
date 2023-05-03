1e7bf7ee8cc284a2d04e3e0b392ecb9d1d8b12fd
Sebastian009w <ia3266595@gmail.com>
2023-05-03 17:50:02 -0500

    with con sqlite

---
+++ database/with.py
@@ -0,0 +1,12 @@
+import _sqlite3
+
+with _sqlite3.connect("database/app.db") as conn:
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+            CREATE TABLE if not exists usuarios
+            (id INTENGER primary key, nombre VARCHAR(50));
+            """
+    )
